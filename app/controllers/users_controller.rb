1eaf9af6fe01ba960548029a4741cbaf2b7b4a35
mihokarakita <miho_kara3@yahoo.co.jp>
2016-05-17 04:21:56 +0000

    kadai1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,49 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :edit]
+  before_action :check_user, only: [:update, :edit]
  
- def show
-   @user = User.find(params[:id])
+ 
+  def show
   end
  
   def new
     @user = User.new
   end
+  
+  def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: '情報を編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
-def create
-  @user = User.new(user_params)
-   if @user.save
-     flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
-   else
-    render'new'
-   end
- end
+  def create
+    @user = User.new(user_params)
+     if @user.save
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+     else
+       render'new'
+     end
+  end
  
- private
+  private
  
- def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
- end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :area)
+  end
+  
+  def check_user
+    redirect_to root_url if @user != current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false}
     has_secure_password
+    validates :profile,presence:true,length:{ maximum:100}, on: :update
+    validates :area,presence:true,length:{ maximum:30}, on: :update
 end
